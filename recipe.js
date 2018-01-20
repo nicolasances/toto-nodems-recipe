3efb7e0f8078df7176b0a998384b894c01f6e67f
nicolas <nicolas.matteazzi@gmail.com>
2018-01-20 15:44:22 +0100

    API Paths

--- recipe.js
+++ recipe.js
@@ -29,19 +29,19 @@ app.use(bodyParser.json());
 
 app.get('/', function(req, res) {res.send({status: 'running'});});
 app.get('/recipes', function(req, res) {getRecipesDlg.getRecipes().then(function(result) {res.send(result);});});
-app.post('/recipe', function(req, res) {postRecipeDlg.postTax(req.body).then(function(result) {res.send(result);});});
+app.post('/recipes', function(req, res) {postRecipeDlg.postTax(req.body).then(function(result) {res.send(result);});});
 app.get('/recipes/:id', function(req, res) {getRecipeDlg.getRecipe(req.params.id).then(function(result) {res.send(result);});});
-app.put('/recipe/:id', function(req, res) {putRecipeDlg.putRecipe(req.params.id, req.body).then(function(result) {res.send(result);});});
+app.put('/recipes/:id', function(req, res) {putRecipeDlg.putRecipe(req.params.id, req.body).then(function(result) {res.send(result);});});
 
 app.get('/recipes/:id/ingredients', function(req, res) {getIngredientsDlg.getIngredients(req.params.id).then(function(result) {res.send(result);});});
-app.post('/recipe/:id/ingredients', function(req, res) {postIngredientDlg.postIngredient(req.params.id, req.body).then(function(result) {res.send(result);});});
+app.post('/recipes/:id/ingredients', function(req, res) {postIngredientDlg.postIngredient(req.params.id, req.body).then(function(result) {res.send(result);});});
 app.delete('/recipes/:id/ingredients/:iid', function(req, res) {deleteIngredientDlg.deleteIngredient(req.params.iid).then(function(result) {res.send(result);});});
-app.put('/recipe/:id/ingredients/:iid', function(req, res) {putIngredientDlg.putIngredient(req.params.iid, req.body).then(function(result) {res.send(result);});});
+app.put('/recipes/:id/ingredients/:iid', function(req, res) {putIngredientDlg.putIngredient(req.params.iid, req.body).then(function(result) {res.send(result);});});
 
 app.get('/recipes/:id/steps', function(req, res) {getStepsDlg.getSteps(req.params.id).then(function(result) {res.send(result);});});
-app.post('/recipe/:id/steps', function(req, res) {postStepDlg.postStep(req.params.id, req.body).then(function(result) {res.send(result);});});
+app.post('/recipes/:id/steps', function(req, res) {postStepDlg.postStep(req.params.id, req.body).then(function(result) {res.send(result);});});
 app.delete('/recipes/:id/steps/:iid', function(req, res) {deleteStepDlg.deleteStep(req.params.iid).then(function(result) {res.send(result);});});
-app.put('/recipe/:id/steps/:iid', function(req, res) {putStepDlg.putStep(req.params.iid, req.body).then(function(result) {res.send(result);});});
+app.put('/recipes/:id/steps/:iid', function(req, res) {putStepDlg.putStep(req.params.iid, req.body).then(function(result) {res.send(result);});});
 
 app.listen(8080, function() {
   console.log('Recipe Microservice up and running');
